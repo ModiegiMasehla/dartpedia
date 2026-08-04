67cf626b154c6067bbbdcaecfe0ad94ee24f7b01
momaseec025 <momaseec025@student.wethinkcode.co.za>
2026-08-04 11:44:02 +0200

    Build command handlers with if statements Work with lists and variables Account for nullability in your code Define and call functions Read and handle user input

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,49 @@
+//core library that provides API
+import 'dart:io'; 
 
 
+//variable for the version
+const version = '0.0.1'; 
+
+//main function checks for version argument
 void main(List<String> arguments) {
-  print('Hello, Dart!'); 
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+     searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+//search command running the core logic of the application
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
+
+
+
+//this function makes the output ore user-friendly, 
+void printUsage() { 
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
 }
